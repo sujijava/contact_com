c4588c139727250d4e65fb0249eccd04cd36f79a
susieoh8@gmail.com <59837559+sujijava@users.noreply.github.com>
2021-04-29 14:05:49 -0400

    deploy ready

--- server/index.js
+++ server/index.js
@@ -4,7 +4,49 @@ import mongoose from 'mongoose'
 import cors from 'cors'
 import listRoutes from './routes/lists.js'
 import path from 'path'
-import dotenv from 'dotenv'
+
+const app = express()
+const __dirname = path.resolve()
+
+app.use(bodyParser.json({}))
+app.use(bodyParser.urlencoded({}))
+app.use(cors())
+
+app.use('/contacts', listRoutes)
+
+//Serve static assets if in production
+// if (process.env.NODE_ENV === 'production') {
+//set static folder
+app.use(express.static('client/build'))
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+})
+// } else {
+//   app.get('/', (req, res) => {
+//     res.send('API is running')
+//   })
+// }
+
+const CONNECTION_URL =
+  'mongodb+srv://dbUser:admin123123@cluster0.g3lyb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+
+const PORT = process.env.PORT || 5000
+
+mongoose
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port`)))
+  .catch((error) => console.log(`${error} did not connect`))
+
+mongoose.set('useFindAndModify', false)
+
+/** development
+ *import express from 'express'
+import bodyParser from 'body-parser'
+import mongoose from 'mongoose'
+import cors from 'cors'
+import listRoutes from './routes/lists.js'
+import path from 'path'
 
 const app = express()
 const __dirname = path.resolve()
@@ -41,3 +83,5 @@ mongoose
   .catch((error) => console.log(`${error} did not connect`))
 
 mongoose.set('useFindAndModify', false)
+
+**/
